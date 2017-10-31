256f56bc0d5ba90c6be84ab72217db0eddcc4de2
evandrocoan <evandrocoan@hotmail.com>
2017-10-31 19:48:58 -0200

    Added initial files.

---
+++ keep_pasted_text_selected.py
@@ -0,0 +1,38 @@
+
+
+import sublime
+import sublime_plugin
+
+
+class SampleListener(sublime_plugin.EventListener):
+
+    def __init__(self):
+        self.start_selection = []
+
+    def on_text_command(self, view, command, args):
+        # print ("About to execute " + command)
+
+        if command == "paste_and_indent":
+
+            for selection in view.sel():
+                self.start_selection.append( selection.begin() )
+
+    def on_post_text_command(self, view, command, args):
+        # print ("Finished executing " + command)
+
+        if command == "paste_and_indent":
+            index = -1
+
+            selections        = view.sel()
+            selections_count  = len( self.start_selection )
+            regions_to_create = []
+
+            for selection in selections:
+                index += 1
+
+                if index < selections_count:
+                    regions_to_create.append( sublime.Region( self.start_selection[index], selection.end() ) )
+
+            for region in regions_to_create:
+                selections.add( region )
+
