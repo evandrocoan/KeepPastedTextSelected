9f0af66125d59b8fbfb58bd41767e690aebe2584
evandrocoan <evandrocoan@hotmail.com>
2018-02-15 23:22:59 -0200

    Fixed selected paste not working with multi-selections

--- keep_pasted_text_selected.py
+++ keep_pasted_text_selected.py
@@ -6,11 +6,15 @@ import sublime_plugin
 
 class SampleListener(sublime_plugin.EventListener):
 
+    def __init__(self):
+        super().__init__()
+        self.start_selection = []
+
     def on_text_command(self, view, command, args):
         # print ("About to execute " + command)
 
         if command == "paste_and_indent":
-            self.start_selection = []
+            self.start_selection.clear()
 
             for selection in view.sel():
                 self.start_selection.append( selection.begin() )
@@ -20,8 +24,9 @@ class SampleListener(sublime_plugin.EventListener):
 
         if command == "paste_and_indent":
             index = -1
+            selections = view.sel()
 
-            selections        = view.sel()
+            selection_offset  = 0
             selections_count  = len( self.start_selection )
             regions_to_create = []
 
@@ -29,7 +34,11 @@ class SampleListener(sublime_plugin.EventListener):
                 index += 1
 
                 if index < selections_count:
-                    regions_to_create.append( sublime.Region( self.start_selection[index], selection.end() ) )
+                    selected_region = sublime.Region( selection_offset + self.start_selection[index], selection.end() )
+                    regions_to_create.append( selected_region )
+
+                selection_offset = selection.end() - self.start_selection[index]
+                # print( "selection_offset:", selection_offset )
 
             for region in regions_to_create:
                 selections.add( region )
